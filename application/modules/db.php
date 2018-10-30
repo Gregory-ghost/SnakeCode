0d0a8b1839e3cc30928c1c64d36de126e1d97b74
a4k <vsekanal@gmail.com>
2018-10-30 09:23:44 +0400

    changes

--- application/modules/db.php
+++ application/modules/db.php
@@ -21,23 +21,35 @@ class DB {
     }
 
     public function getUsers() {
-        $query = 'SELECT * FROM user';
+        $query = 'SELECT * FROM user ORDER BY id DESC';
         return $this->db->query($query)->fetchAll(PDO::FETCH_CLASS);
     }
-    public function getUser($login) {
-        $field = $this->getField($login);
-        $query = 'SELECT * FROM user WHERE login="'.$login.'"';
+    public function getUserByLogin($login) {
+        $query = 'SELECT * FROM user WHERE login="'.$login.'" ORDER BY id DESC LIMIT 1';
+        return $this->db->query($query)->fetchObject('stdClass');
+    }
+    public function getUserByToken($login) {
+        $query = 'SELECT * FROM user WHERE token="'.$login.'" ORDER BY id DESC LIMIT 1';
+        return $this->db->query($query)->fetchObject('stdClass');
+    }
+    public function getUser($login, $password) {
+        $query = 'SELECT * FROM user WHERE login="'.$login.'" password="'.$password.'"';
         return $this->db->query($query)->fetchObject('stdClass');
     }
     public function getSnakes() {
         $query = 'SELECT * FROM snake';
         return $this->db->query($query)->fetchAll(PDO::FETCH_CLASS);
     }
+    public function getSnakesFromUser($user_id) {
+        $query = 'SELECT * FROM snake, user WHERE user.id="'.$user_id.'" AND snake.user_id=user.id ORDER BY snake.id DESC';
+        return $this->db->query($query)->fetchAll(PDO::FETCH_CLASS);
+    }
+    public function getSnakeById($id) {
+        $query = 'SELECT * FROM snake WHERE id="'.$id.'"';
+        return $this->db->query($query)->fetchObject('stdClass');
+    }
     public function getFoods() {
         $query = 'SELECT * FROM food';
         return $this->db->query($query)->fetchAll(PDO::FETCH_CLASS);
-    }
-    private function getField($field) {
-        return "`".str_replace("`","``",$field)."`";
     }
 }
