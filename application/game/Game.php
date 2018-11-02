8acf9b93a0452c2c64e458a10509cb319617d501
Sofia <43928666+SofiaLol@users.noreply.github.com>
2018-11-02 20:24:35 +0300

    Add files via upload

--- application/game/Game.php
+++ application/game/Game.php
@@ -28,4 +28,17 @@ class Game {
     public function getStruct() {
         return $this->struct;
     }
+    public function startSession() {
+        if ( session_id() ) return true;
+        else return session_start();
+    }
+    public function destroySession() {
+        if ( session_id() ) {
+            // Если есть активная сессия, удаляем куки сессии,
+            setcookie(session_name(), session_id(), time()-60*60*24);
+            // и уничтожаем сессию
+            session_unset();
+            session_destroy();
+        }
+    }
 }
